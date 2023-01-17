d9c70cea6a8ece6e686e8cc862cb9bcc5ed05974
mxz50 <mxz50@MSI>
2023-01-16 23:26:18 -0800

    fix plotting

--- DSP/NyquistIsBad.py
+++ DSP/NyquistIsBad.py
@@ -13,30 +13,31 @@ import numpy as np
 from matplotlib import pyplot as plt 
 
 def main():
-    srate=1000
-    t=np.arange(0,1,1/srate)
-    npnts=len(t)
-    f=10
-    signal=np.sin(2*np.pi*t*f)
+    Fs=1000
+    t_res = 1/Fs
+    t=np.arange(0,1,t_res)
+    N=len(t)
+    f = 10
+    x=np.sin(2*np.pi*t*f)
     
-    # measurement rates
-    srates = [10,20,50,100]
-    # srates=[15]
+    # sampling rates
+    sample_rates = [10,20,50,100]
     plots=[(0,0),(0,1),(1,0),(1,1)]
     
-    for j in range(len(srates)):
+    for j in range(len(sample_rates)):
         # print(rate)
-        T=1/srates[j]
-        midx=np.zeros(srates[j],dtype=int)
-        mtime=np.arange(0,1,1/srates[j])
-        for i in range(srates[j]):
+        T=1/sample_rates[j]
+        midx=np.zeros(sample_rates[j],dtype=int)
+        mtime=np.arange(0,1,1/sample_rates[j])
+        for i in range(sample_rates[j]):
             midx[i]=np.argmin(abs(mtime[i]-t))
         
         plt.subplot2grid((2,2),plots[j])
-        plt.stem(t[midx],signal[midx])
-        plt.plot(t,signal,'r')
-        plt.title(f"Sampled at {srates[j]/f}")
+        plt.stem(t[midx],x[midx])
+        plt.plot(t,x,'r')
+        plt.title(f"Sampled at {sample_rates[j]/f}")
         plt.tight_layout()
         plt.xlim(0,.3)
+    plt.show()
         
 main()
